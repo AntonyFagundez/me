40b0fcbd1327f5c77104b37f9ca2a5bf96deb897
Antony Fagundez <jhonfagundez@gmail.com>
2022-03-26 17:44:34 -0300

    feat: add alphabet

---
+++ src/alphabet.ts
@@ -0,0 +1,9 @@
+const katakana = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤャユュヨョラリルレロワヰヱー";
+
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+
+
+const alphabet = latin + numbers + katakana;
+
+export default alphabet;
