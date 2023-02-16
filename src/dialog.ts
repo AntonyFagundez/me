60422f1b2028f5e5f14198478703747321a133d0
Antony Fagundez <jhonfagundez@gmail.com>
2023-02-15 23:26:38 -0300

    feat: dialog centralized

---
+++ src/dialog.ts
@@ -0,0 +1,43 @@
+import { subscribeKey } from "valtio/utils";
+
+import { dragElement } from "./draggable";
+import { InputManager } from "./InputManager";
+import { store } from "./store";
+
+const button = document.getElementById("init-button") as HTMLButtonElement;
+const dialog = document.getElementById("dialog") as HTMLDialogElement;
+const main = document.getElementById("main") as HTMLElement;
+
+// subscribeKey(store, "openDialog", () => dialog.open = store.openDialog)
+
+subscribeKey(store.dialog, "isOpen", (value) => {
+  if (store.dialog.isOpen) {
+    button.style.display = "none";
+  } else {
+    button.style.display = "block";
+  }
+});
+
+subscribeKey(store.dialog, "isOpen", (value) => {
+  main.setAttribute("data-initialized", value.toString());
+
+  dialog.open = value;
+  if (value) {
+    const closeButton = document.getElementById("close-dialog") as HTMLButtonElement;
+
+    closeButton.addEventListener("click", () => {
+      store.dialog.isOpen = false;
+    });
+
+    new InputManager();
+
+    dragElement(
+      document.getElementById("dialog") as HTMLElement,
+      document.getElementById("actions") as HTMLDivElement
+    );
+  }
+});
+
+button.addEventListener("click", () => {
+  store.dialog.isOpen = !store.dialog.isOpen;
+});
